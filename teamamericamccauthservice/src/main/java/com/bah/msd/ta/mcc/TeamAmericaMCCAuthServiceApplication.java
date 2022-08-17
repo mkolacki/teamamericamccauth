51816fd23d11d3640e4ee562036a1c6601297faf
user <user@this.com>
2022-08-17 11:50:18 -0400

    first commit

---
+++ teamamericamccauthservice/src/main/java/com/bah/msd/ta/mcc/TeamAmericaMCCAuthServiceApplication.java
@@ -0,0 +1,13 @@
+package com.bah.msd.ta.mcc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamAmericaMCCAuthServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeamAmericaMCCAuthServiceApplication.class, args);
+	}
+
+}
